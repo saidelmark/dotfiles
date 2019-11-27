8afe7a7c0d29a15b9643a1ffd084c324792fca93
Mark Zaidelman <saidelman@ya.ru>
2019-11-27 23:19:47 +0300

    add zsh config

---
+++ .zsh/alias.sh
@@ -0,0 +1,15 @@
+# General
+alias _='sudo'
+alias dotfiles='git --git-dir=$HOME/dotfiles.git'
+
+# Editing
+## WM and hotkeys
+alias cfbsp='$EDITOR ~/.config/bspwm/bspwmrc'
+alias cfsxhk='$EDITOR ~/.config/sxhkd/sxhkdrc'
+alias cfpb='$EDITOR ~/.config/polybar/config'
+alias cfcomp='$EDITOR ~/.config/compton/compton.conf'
+
+## Zsh
+alias cfz='$EDITOR ~/.zshrc'
+alias cfza='$EDITOR ~/.zsh/alias.sh'
+
