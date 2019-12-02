8ec92c25b6cd1731740b7da5dd2cbdc770972516
Mark Zaidelman <saidelman@ya.ru>
2019-12-02 20:33:54 +0300

    Tweak the appearance a bit

--- .config/rofi/scripts/powermenu.sh
+++ .config/rofi/scripts/powermenu.sh
@@ -16,23 +16,29 @@ options="$power_off\n$reboot\n$lock\n$suspend\n$log_out"
 chosen="$(echo -e "$options" | $rofi_command -dmenu -selected-row 2)"
 case $chosen in
     $power_off)
+      pactl set-source-mute @DEFAULT_SOURCE@ true
+      pactl set-source-mute @DEFAULT_SOURCE@ true
       systemctl poweroff
       ;;
     $reboot)
+      pactl set-source-mute @DEFAULT_SOURCE@ true
       systemctl reboot
       ;;
     $lock)
+      pactl set-source-mute @DEFAULT_SOURCE@ true
       sleep 1
       scrot --silent /tmp/screen.png; \
       convert /tmp/screen.png -wave 8x32 -blur 0x3 -paint 3 /tmp/screen.png; \
       i3lock -e -f -i /tmp/screen.png
       ;;
     $suspend)
-      mpc -q pause
-      amixer set Master mute
-      systemctl suspend
+      pactl set-sink-mute @DEFAULT_SINK@ true
+      pactl set-source-mute @DEFAULT_SOURCE@ true
+      # systemctl suspend
+      sudo hibernate
       ;;
     $log_out)
+      pactl set-source-mute @DEFAULT_SOURCE@ true
       bspc quit
       ;;
 esac
