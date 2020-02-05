b00c8d7eaaad62ccf8bcb3a445b7e7139cf47b46
Mark Zaidelman <saidelman@ya.ru>
2020-02-05 17:32:43 +0300

    Update screenshots script
    Now the newly created screenshot is being copied to clipboard.

--- .local/bin/dmenu_screenshot.sh
+++ .local/bin/dmenu_screenshot.sh
@@ -4,7 +4,7 @@
 timeout=0
 
 declare -a options=(" an area 
- a window 
+ current window 
  the whole screen ")
 
 declare -a timeoutOptions=("3
@@ -12,8 +12,9 @@ declare -a timeoutOptions=("3
 10")
 
 choice=$(echo -e "${options[@]}" | rofi -dmenu -i -p 'Make a screenshot of ')
-fileName='%Y-%m-%d_%h-%M-%S_$wx$h.png'
-destination='mv $f ~/Pictures/screenshots'
+filePattern='%Y-%m-%d_%h-%M-%S_$wx$h.png'
+destination="$HOME/Pictures/screenshots"
+postCommand='mv $f'
 
 if [ "$1" ]
 then
@@ -22,16 +23,19 @@ fi
 
 case "$choice" in
   ' an area ')
-    scrot --silent --select "$fileName" --exec "$destination" --delay "$timeout" --count
+    scrot --silent --select "$filePattern" --exec "$postCommand $destination" --delay "$timeout" --count
     ;;
   ' current window ')
-    scrot --focused --silent "$fileName" --exec "$destination" --delay "$timeout" --count
+    scrot --focused --silent "$filePattern" --exec "$postCommand $destination" --delay "$timeout" --count
     ;;
   ' the whole screen ')
-    scrot --silent "$fileName" --exec "$destination" --delay "$timeout" --count
+    scrot --silent "$filePattern" --exec "$postCommand $destination" --delay "$timeout" --count
     ;;
   *)
     echo "Unknown command"
     ;;
 esac
 
+# Put the image to clipboard so I don't have to do it manually
+fileName=$(ls -t "$destination" | head -n 1)
+xclip -selection clipboard -t image/png -i "$destination/$fileName"
