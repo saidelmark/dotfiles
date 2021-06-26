c9245a8b37532a07f4fe78a24d53301bfdab17c8
Mark Zaidelman <saidelman@ya.ru>
2021-06-26 13:21:36 +0300

    bspwm: add a script to configure monitors
    This is intended to finally unify this part of config files on all of my
    computers. This time I only added akvelon-issued laptop, others are TBD

---
+++ bspwm/.config/bspwm/monitor-config.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+akvelon_hp() {
+  if xrandr --listactivemonitors | grep -q 'HDMI1'; then
+    xrandr --output HDMI1 --right-of eDP1 --primary
+    bspc monitor HDMI1 -d 1 2 3 4 slack 6 mail 8
+    bspc monitor eDP1 -d chat personal
+    bspc wm --reorder-monitors HDMI1 eDP1
+  else
+    bspc monitor eDP1 -d 1 2 3 4 slack 6 mail 8 chat personal
+  fi
+}
+
+default() {
+  bspc monitor ^1 -d 1 2 3 4 5 6 7 8 9 0
+}
+
+case "$(hostname)" in
+  "n71") akvelon_hp ;;
+  *)     default ;;
+esac
+
