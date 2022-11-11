9645ae6fd43530d49571267018e5bee436ef1146
Mark Zaidelman <saidelman@ya.ru>
2022-11-11 18:56:24 +0400

    QoL: don't notify at full battery

--- QoL/.local/bin/battery-notify.sh
+++ QoL/.local/bin/battery-notify.sh
@@ -3,10 +3,7 @@
 CAPACITY=$(cat /sys/class/power_supply/BAT0/capacity)
 STATUS=$(cat /sys/class/power_supply/AC/online)
 
-if [[ (${CAPACITY} -gt 80) && (${STATUS} -eq 1)]]
-then
-  dunstify -u critical -a "battery" -i "/usr/share/icons/Adwaita/96x96/legacy/battery-full-symbolic.symbolic.png" -h "int:value:$CAPACITY" "Battery is above 80%"
-elif [[ (${CAPACITY} -lt 20) && (${STATUS} -eq 0)]]
+if [[ (${CAPACITY} -lt 20) && (${STATUS} -eq 0)]]
 then
   dunstify -u critical -a "battery" -i "/usr/share/icons/Adwaita/96x96/legacy/battery-low-symbolic.symbolic.png" -h "int:value:$CAPACITY" "Battery is below 20%"
 fi
