a2f0e77d1cb00359fd15bbca3b405cb59372bfc3
Mark Zaidelman <saidelman@ya.ru>
2023-01-26 17:24:09 +0400

    vim: change mappings for completion window navigation

--- vim/.config/nvim/after/plugin/cmp.lua
+++ vim/.config/nvim/after/plugin/cmp.lua
@@ -12,8 +12,8 @@ cmp.setup({
     completion = cmp.config.window.bordered(),
   },
   mapping = {
-    ['<C-k>'] = cmp.mapping.select_prev_item(),
-    ['<C-j>'] = cmp.mapping.select_next_item(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
     -- Add tab support
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
