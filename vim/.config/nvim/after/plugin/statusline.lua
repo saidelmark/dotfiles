e69b6e3baecadfadf88a6b543d22b345830a3675
Mark Zaidelman <saidelman@ya.ru>
2023-02-01 22:03:01 +0400

    vim: enable tabline
    TODO: disable it when only one tab is open

--- vim/.config/nvim/after/plugin/statusline.lua
+++ vim/.config/nvim/after/plugin/statusline.lua
@@ -9,4 +9,40 @@ require('lualine').setup({
   inactive_sections = {
     lualine_b = { 'diff', 'diagnostics' },
   },
+
+  tabline = {
+    lualine_a = {
+      {
+        'windows',
+        windows_color = {
+          active = 'lualine_a_normal',
+          inactive = 'lualine_b_inactive',
+        },
+        disabled_buftypes = { 'quickfix', 'prompt', 'lua' },
+      }
+    },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {
+      {
+        'tabs',
+        max_length = vim.o.columns / 3,
+        mode = 2,
+        tabs_color = {
+          active = 'lualine_a_normal',
+          inactive = 'lualine_b_inactive',
+        },
+        fmt = function(name, context)
+          local buflist = vim.fn.tabpagebuflist(context.tabnr)
+          local winnr = vim.fn.tabpagewinnr(context.tabnr)
+          local bufnr = buflist[winnr]
+          local mod = vim.fn.getbufvar(bufnr, '&mod')
+          return name .. (mod == 1 and ' +' or '')
+        end
+      }
+    },
+  },
+  winbar = {},
 })
