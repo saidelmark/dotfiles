5e76971fa6fc7c0e3d6a9f387b3f4d0948e5a084
Mark Zaidelman <saidelman@ya.ru>
2024-03-03 01:12:20 +0300

    rofi: Use better icons for powermenu

--- rofi/.config/rofi/scripts/powermenu.sh
+++ rofi/.config/rofi/scripts/powermenu.sh
@@ -5,8 +5,8 @@ rofi_command="rofi -theme themes/powermenu.rasi"
 ### Options ###
 power_off=""
 reboot=""
-lock=""
-suspend="鈴"
+lock=""
+suspend=""
 log_out=""
 # Variable passed to rofi
 options="$power_off\n$reboot\n$lock\n$suspend\n$log_out"
