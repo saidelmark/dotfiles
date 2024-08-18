0735bce3a7000765abc984c27f308790324c1ad3
Mark Zaidelman <saidelman@ya.ru>
2024-08-18 13:14:07 +0200

    qol: Migrate volume control to pipewire

--- QoL/.local/bin/volume.sh
+++ QoL/.local/bin/volume.sh
@@ -4,21 +4,21 @@
 msgId="991049"
 
 get_volume() {
-  pulseaudio-ctl full-status | awk '{print $1}'
+  wpctl get-volume @DEFAULT_SINK@ | awk '{print 100 * $2}'
 }
 
 is_mute() {
-  pulseaudio-ctl full-status | awk '{print $2}'
+  wpctl get-volume @DEFAULT_SINK@ | awk '{print $3}'
 }
 
 is_mute_input() {
-  pulseaudio-ctl full-status | awk '{print $3}'
+  wpctl get-volume @DEFAULT_SOURCE@ | awk '{print $3}'
 }
 
 notify() {
   if [ "$1" = "mic" ]
   then
-    if [ "$(is_mute_input)" = "yes" ]; then
+    if [ "$(is_mute_input)" = "[MUTED]" ]; then
       icon="/usr/share/icons/Adwaita/symbolic/status/microphone-sensitivity-muted-symbolic.svg"
     else
       icon="/usr/share/icons/Adwaita/symbolic/status/microphone-sensitivity-medium-symbolic.svg"
@@ -26,17 +26,23 @@ notify() {
   else
     volume=$(get_volume)
     mute=$(is_mute)
-    if [ "$mute" = "yes" ]; then
+    if [ "$mute" = "[MUTED]" ]; then
+      echo "muted"
       icon="/usr/share/icons/Adwaita/symbolic/status/audio-volume-muted-symbolic.svg"
     elif [ "$volume" -eq "0" ]; then
+      echo "zero volume"
       icon="/usr/share/icons/Adwaita/symbolic/status/audio-volume-muted-symbolic.svg"
     elif [ "$volume" -lt "40" ]; then
+      echo "low volume"
       icon="/usr/share/icons/Adwaita/symbolic/status/audio-volume-low-symbolic.svg"
     elif [ "$volume" -lt "60" ]; then
+      echo "mid volume"
       icon="/usr/share/icons/Adwaita/symbolic/status/audio-volume-medium-symbolic.svg"
     elif [ "$volume" -lt "90" ]; then
+      echo "high volume"
       icon="/usr/share/icons/Adwaita/symbolic/status/audio-volume-high-symbolic.svg"
     else
+      echo "too high volume"
       icon="/usr/share/icons/Adwaita/symbolic/status/audio-volume-overamplified-symbolic.svg"
     fi
   fi
@@ -47,19 +53,19 @@ notify() {
 
 case "$1" in
   "up")
-    pulseaudio-ctl up
+    wpctl set-volume -l 1.0 @DEFAULT_SINK@ 5%+
     notify
     ;;
   "down")
-    pulseaudio-ctl down
+    wpctl set-volume -l 1.0 @DEFAULT_SINK@ 5%-
     notify
     ;;
   "mute")
-    pulseaudio-ctl mute
+    wpctl set-mute @DEFAULT_SINK@ toggle
     notify
     ;;
   "mute-input")
-    pulseaudio-ctl mute-input
+    wpctl set-mute @DEFAULT_SOURCE@ toggle
     notify mic
     ;;
   *)
