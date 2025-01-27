21f735ac5a21d46c345c9c39324e82a324e7b4bd
Mark Zaidelman <saidelman@ya.ru>
2025-01-27 21:13:34 +0100

    vim: wrapper for keymaps in folding plugin
    And increase default foldlevel

--- vim/.config/nvim/after/plugin/ufo.lua
+++ vim/.config/nvim/after/plugin/ufo.lua
@@ -1,17 +1,19 @@
 local ufo = require('ufo')
+local set_keymap = require('saidelman.functions').set_keymap
 
 vim.o.foldcolumn = '0'
-vim.o.foldlevel = 9
-vim.o.foldlevelstart = 9
+vim.o.foldlevel = 15
+vim.o.foldlevelstart = 15
 vim.o.foldenable = true
-vim.keymap.set('n', 'zR', ufo.openAllFolds)
-vim.keymap.set('n', 'zM', ufo.closeAllFolds)
+set_keymap('zR', ufo.openAllFolds, "Folds: open all")
+set_keymap('zM', ufo.closeAllFolds, "Folds: close all")
 -- This doesn't work, ufo plugin currently has no support for vim's 'zm' and 'zr' behaviour
 -- vim.keymap.set('n', 'zr', ufo.openFoldsExceptKinds)
 -- 'zm' is partially supported, though '2zm', '7zm'
-vim.keymap.set('n', 'zm', ufo.closeFoldsWith, {})
+set_keymap('zm', ufo.closeFoldsWith, "Folds: close N levels")
 
 ufo.setup({
+---@diagnostic disable-next-line: unused-local
   provider_selector = function(bufnr, filetype, buftype)
     return { 'treesitter', 'indent' }
   end
