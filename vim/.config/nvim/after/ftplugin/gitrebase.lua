e80414e5cbf336c09e00a24625412537454953d4
Mark Zaidelman <saidelman@ya.ru>
2025-01-27 21:11:13 +0100

    vim: Use wrapper for keymaps in rebasse and for undotree

--- vim/.config/nvim/after/ftplugin/gitrebase.lua
+++ vim/.config/nvim/after/ftplugin/gitrebase.lua
@@ -1,12 +1,9 @@
-local function set_buf_keymap(map, cmd)
-  vim.keymap.set('n', map, cmd, { buffer = true, silent = true, noremap = true })
-end
+local set_keymap = require('saidelman/functions').set_keymap
 
-set_buf_keymap('<localleader>p', '0ciwpick<esc>0')
-set_buf_keymap('<localleader>r', '0ciwreword<esc>0')
-set_buf_keymap('<localleader>d', '0ciwdrop<esc>0')
-set_buf_keymap('<localleader>e', '0ciwedit<esc>0')
-set_buf_keymap('<localleader>s', '0ciwsquash<esc>0')
-set_buf_keymap('<localleader>f', '0ciwfixup<esc>0')
-set_buf_keymap('<localleader>x', 'oexec <c-g>u')
-set_buf_keymap('<localleader>X', 'Oexec <c-g>u')
+set_keymap('<localleader>p', '0ciwpick<esc>0', 'Rebase: Pick this commit')
+set_keymap('<localleader>d', '0ciwdrop<esc>0', 'Rebase: Drop this commit')
+set_keymap('<localleader>e', '0ciwedit<esc>0', 'Rebase: Edit this commit')
+set_keymap('<localleader>s', '0ciwsquash<esc>0', 'Rebase: Squash this commit')
+set_keymap('<localleader>f', '0ciwfixup<esc>0', 'Rebase: Fixup this commit')
+set_keymap('<localleader>x', 'oexec <c-g>u', 'Rebase: Exec before this commit')
+set_keymap('<localleader>X', 'Oexec <c-g>u', 'Rebase: Exec after this commit')
--- vim/.config/nvim/after/plugin/undotree.lua
+++ vim/.config/nvim/after/plugin/undotree.lua
@@ -1,4 +1,4 @@
-vim.keymap.set('n', '<F12>', ':UndotreeToggle<CR>', { silent = true })
+require('saidelman.functions').set_keymap('<F12>', ':UndotreeToggle<CR>', "Toggle undotree", {buffer = false})
 vim.g.undotree_WindowLayout = 2
 vim.g.undotree_HighlightChangedText = 0
 vim.g.undotree_SetFocusWhenToggle = 1
