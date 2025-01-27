47c24a7c5641080dada33ad85d32ea3c34b01844
Mark Zaidelman <saidelman@ya.ru>
2025-01-27 21:04:36 +0100

    vim: reconfigure fzf plugin
    1. Use my wrapper for keymaps
    2. Get rid of copy-paste default config (not sure why it even was there)
    3. Switch to default profile.
    4. Enable simple commands for some fzf-lua features (Files, Buffers,
       etc)

--- vim/.config/nvim/after/plugin/fzf.lua
+++ vim/.config/nvim/after/plugin/fzf.lua
@@ -1,40 +1,6 @@
-require('fzf-lua').setup({
-  -- The following are keymamp active when fzf window is active
-  'fzf-vim',
-  keymap = {
-    builtin = {
-      -- neovim `:tmap` mappings for the fzf win
-      ['<F1>']  = 'toggle-help',
-      ['<F2>']  = 'toggle-fullscreen',
-      -- Only valid with the 'builtin' previewer
-      ['<F3>']  = 'toggle-preview-wrap',
-      ['<F4>']  = 'toggle-preview',
-      -- Rotate preview clockwise/counter-clockwise
-      ['<F5>']  = 'toggle-preview-ccw',
-      ['<F6>']  = 'toggle-preview-cw',
-      ['<M-j>'] = 'preview-page-down',
-      ['<M-k>'] = 'preview-page-up',
-    },
-    fzf = {
-      -- fzf '--bind=' options
-      ['ctrl-z']     = 'abort',
-      ['ctrl-u']     = 'unix-line-discard',
-      ['ctrl-f']     = 'half-page-down',
-      ['ctrl-b']     = 'half-page-up',
-      ['ctrl-a']     = 'beginning-of-line',
-      ['ctrl-e']     = 'end-of-line',
-      ['alt-a']      = 'toggle-all',
-      -- Only valid with fzf previewers (bat/cat/git/etc)
-      ['f3']         = 'toggle-preview-wrap',
-      ['f4']         = 'toggle-preview',
-      -- scrolling the preview
-      ['shift-down'] = 'preview-page-down',
-      ['shift-up']   = 'preview-page-up',
-      ['alt-j']      = 'preview-down',
-      ['alt-k']      = 'preview-up',
-    },
-  },
-})
+require('fzf-lua').setup({})
+require('fzf-lua').setup_fzfvim_cmds()
 
-vim.keymap.set('n', '<C-M-P>', ':FzfLua commands<CR>', { silent = true })
-vim.keymap.set('n', '<C-P>', ':FzfLua files<CR>', { silent = true })
+local set_keymap = require('saidelman.functions').set_keymap
+set_keymap('<C-M-P>', ':FzfLua commands<CR>', "Fzf: Available commands", {buffer = false})
+set_keymap('<C-P>', ':FzfLua files<CR>', "Fzf: Files", {buffer = false})
