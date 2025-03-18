51f5a9d77280a5b72d14cfa80732ea0643276f9c
Mark Zaidelman <saidelman@ya.ru>
2025-03-18 22:30:06 +0100

    vim: Better fzf integration
    1. Use fzf as ui selector! Rare case, but so cool
    2. Change existing keymap from "show commands" to "show files in the
       same directory as current file". This is implemented with fzf query,
       this way it's easy to go up a level

--- vim/.config/nvim/after/plugin/fzf.lua
+++ vim/.config/nvim/after/plugin/fzf.lua
@@ -1,6 +1,15 @@
-require('fzf-lua').setup({})
-require('fzf-lua').setup_fzfvim_cmds()
+local fzf = require('fzf-lua')
+fzf.setup()
+fzf.setup_fzfvim_cmds()
+fzf.register_ui_select()
 
 local set_keymap = require('saidelman.functions').set_keymap
-set_keymap('<C-M-P>', ':FzfLua commands<CR>', "Fzf: Available commands", {buffer = false})
-set_keymap('<C-P>', ':FzfLua files<CR>', "Fzf: Files", {buffer = false})
+set_keymap(
+  '<C-M-P>',
+  function()
+    fzf.files({ fzf_opts = { ['--query'] = vim.fn.expand('%:p:.:h') } })
+  end,
+  "Fzf: Available commands",
+  { buffer = false }
+)
+set_keymap('<C-P>', fzf.files, "Fzf: Files", { buffer = false })
