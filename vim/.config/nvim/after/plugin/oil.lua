c7c5556dc7e16d5c46c09d16610fb9c8118a618c
Mark Zaidelman <saidelman@ya.ru>
2025-07-11 00:24:46 +0200

    vim: Ignore case when sorting oil buffers

--- vim/.config/nvim/after/plugin/oil.lua
+++ vim/.config/nvim/after/plugin/oil.lua
@@ -1,6 +1,7 @@
 vim.keymap.set('n', '-', require('oil').open, { desc = 'Open parent directory' })
 require('oil').setup({
   view_options = {
+    case_insensitive = true,
     show_hidden = true,
   },
 })
