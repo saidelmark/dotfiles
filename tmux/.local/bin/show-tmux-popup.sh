60776c06f40d3caaa26324f0b81e10c03694a86b
Mark Zaidelman <saidelman@ya.ru>
2025-07-23 23:05:30 +0200

    tmux: Open popup in current directory
    To make it compatible with "different windows have different directories
    open", popups are now per-window

--- tmux/.local/bin/show-tmux-popup.sh
+++ tmux/.local/bin/show-tmux-popup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-session="_popup_$(tmux display -p '#S')"
+session="_popup_$(tmux display -p '#S_#W')"
 
 if ! tmux has -t "$session" 2> /dev/null; then
   parent_session="$(tmux display -p '#{session_id}')"
